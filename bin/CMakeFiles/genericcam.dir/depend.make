592fc2765fafac588f3333f97cdbb5226a47ce02
Silver-Snail <jlc29945@gmail.com>
2025-06-25 23:00:29 -0400

    Initial commit

--- bin/CMakeFiles/genericcam.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for genericcam.
-# This may be replaced when dependencies are built.
--- bin/src/test/CMakeFiles/cppunittest.dir/TestSelection.cpp.o.d
+++
@@ -1,5 +0,0 @@
-src/test/CMakeFiles/cppunittest.dir/TestSelection.cpp.o: \
- /home/pigs/genericcam-git/src/test/TestSelection.cpp \
- /usr/include/stdc-predef.h \
- /home/pigs/genericcam-git/src/test/TestSelection.h \
- /home/pigs/genericcam-git/src/test/../Config.h
